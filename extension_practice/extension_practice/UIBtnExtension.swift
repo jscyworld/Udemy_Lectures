f8aad7c7df877f3f97236a80b54095718b41974d
jscyworld <jscyworld@naver.com>
2018-01-19 09:49:56 +0900

    180119 function wiggle modified

--- extension_practice/extension_practice/UIBtnExtension.swift
+++ extension_practice/extension_practice/UIBtnExtension.swift
@@ -12,10 +12,10 @@ extension UIButton {
     func wiggle() {
         let wiggleAnim = CABasicAnimation(keyPath: "position")
         wiggleAnim.duration = 0.05
-        wiggleAnim.repeatCount = 5
+        wiggleAnim.repeatCount = 3
         wiggleAnim.autoreverses = true
-        wiggleAnim.fromValue = CGPoint(x: self.center.x - 4.0, y: self.center.y)
-        wiggleAnim.toValue = CGPoint(x: self.center.x + 4.0, y: self.center.y)
+        wiggleAnim.fromValue = CGPoint(x: self.center.x - 3.0, y: self.center.y)
+        wiggleAnim.toValue = CGPoint(x: self.center.x + 3.0, y: self.center.y)
         layer.add(wiggleAnim, forKey: "position")
     }
     
